09b8ffddb334526e0503bb0489c2e5e1533f8254
marcoshack <marcoshack@35647ce2-ea60-11dd-9059-71fac19e68c6>
2009-06-04 15:08:33 +0000

    Test code removed from RTPProxyConsole.

--- rtpproxy-client-console/src/main/java/org/vtlabs/rtpproxy/console/RTPProxyConsole.java
+++ rtpproxy-client-console/src/main/java/org/vtlabs/rtpproxy/console/RTPProxyConsole.java
@@ -1,37 +1,10 @@
 package org.vtlabs.rtpproxy.console;
 
-import java.io.Console;
-import org.apache.log4j.Logger;
-import org.vtlabs.rtpproxy.client.UDPService;
-
 /**
  *
  * @author Marcos Hack <marcosh@voicetechnology.com.br>
  */
 public class RTPProxyConsole {
-    private static Logger logger = Logger.getLogger(RTPProxyConsole.class);
-
     public static void main(String[] args) {
-        try {
-            Thread.currentThread().setName("UDPServerConsole");
-            
-            UDPService udpServer = new UDPService();
-            udpServer.start();
-
-            Console console = System.console();
-            String command = "";
-            while (!command.equals("exit")) {
-                console.printf("udpserver> ");
-                command = console.readLine();
-
-                if (!(command.equals("") || command.equals("exit"))) {
-                    logger.debug("Command read: " + command);
-                    udpServer.send(command);
-                }
-            }
-
-        } catch (Exception e) {
-            logger.error("Unknow error running UDPServer", e);
-        }
     }
 }
